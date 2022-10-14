bca851fce3ae5d4949dca7a36c2e40e8d7bd50f8
LinhNguyen <linhnq236@gmail.com>
2022-10-14 16:52:31 +0700

    add gem devise

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,18 @@
 module ApplicationHelper
+  def bootstrap_class_for(flash_type)
+    {
+      success: "alert-success",
+      error: "alert-danger",
+      alert: "alert-warning",
+      notice: "alert-info"
+    }.stringify_keys[flash_type.to_s] || flash_type.to_s
+  end
+
+  def is_image type
+    type.include?("image") 
+  end
+
+  def format_datetime datetime
+    datetime.strftime("%Y-%m-%d")
+  end
 end
