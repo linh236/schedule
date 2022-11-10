6ecbf738d912c4c0c5f4aff8ee358a9e2b77d680
LinhNguyen <linhnq236@gmail.com>
2022-11-10 18:55:59 +0700

    remove attandance

--- app/controllers/admin/dashboard_controller.rb
+++ app/controllers/admin/dashboard_controller.rb
@@ -6,8 +6,5 @@ class Admin::DashboardController < AdminController
     start_date = params.fetch(:start_date, Date.today).to_date
     @events = Event.only_events.where(start_time: start_date.beginning_of_month.beginning_of_week..start_date.end_of_month.end_of_week, user: current_user)
     @selectedDate = params[:selectedDate] if params[:selectedDate].present?
-    if user_signed_in?
-      @attendance  = AttendanceLog.find_by(user_id: current_user.id, date: DATETIME.to_date)
-    end
   end
 end
